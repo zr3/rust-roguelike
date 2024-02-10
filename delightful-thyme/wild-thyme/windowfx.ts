a5bb89985002efd6d2abcb318748a9db52de7abe
zr3 <zak.reynolds+github@zakjr.com>
2024-02-09 23:40:07 -0800

    fix: updated tutorial text

--- delightful-thyme/wild-thyme/windowfx.ts
+++ delightful-thyme/wild-thyme/windowfx.ts
@@ -233,7 +233,7 @@ function startGame() {
     wasm_bindgen("./wasm/and_we_had_a_wild_thyme_bg.wasm");
 }
 async function tutorialText() {
-    await spellOutText("Well, hopefully I can remember that! Anyway, you are the @, and time only moves when you move. ENTER to see what the symbols on-screen are. SPACE to interact with things, and more controls below. DYING IS NORMAL! This world is about exploration and experimentation, and there are plenty of items to find, so don't be afraid to use them!");
+    await spellOutText("Ah, hopefully I can remember that.. Anyway, YOU are the @, and time only moves when you move. Press SPACE to interact with things, and see more controls below. You will explore the forest to find ingredients for a cake, and DYING IS NORMAL! This world is about exploration and experimentation. There is an abundance of items to find, so don't be afraid to use them..");
     document.getElementById("bottom-text")?.classList.remove('hidden');
 }
 if (window.location.search.includes('debug')) {
